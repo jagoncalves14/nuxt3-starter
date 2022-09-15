b503ff00f4b9072a3f4421a2583fe527743188c2
João Gonçalves <jandretgoncalves@gmail.com>
2022-09-15 22:07:29 +0100

    Improve eslint rules

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,13 @@ useHead({
 </script>
 
 <template>
-  <main>
+  <main class="mt-8">
     <h1 class="text-3xl font-black text-white">Hi 👋</h1>
     <p class="mt-4">Welcome to the Homepage.</p>
     <p class="mt-4">
       I'm going to play around with Nuxt here. This app uses Supabase for Authentication Nuxt's Content Module.
     </p>
+
     <div class="flex gap-4 mt-8">
       <NuxtLink to="/login" class="px-4 py-2 rounded bg-charcoal-600 hover:bg-charcoal-500">
         Check your profile
